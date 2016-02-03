103bd3595b4fba16cec0a3b7ee6af6cc27498190
EricSM <eric_miramontes@outlook.com>
2016-02-03 00:04:25 -0700

    Added UnitTest project for DependencyGraph

---
+++ Spreadsheet/DependencyGraphTests/DependencyGraphTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dependencies;
+
+namespace DependencyGraphTests
+{
+    [TestClass]
+    public class DependencyGraphTests
+    {
+        [TestMethod]
+        public void TestConstructor1()
+        {
+            DependencyGraph dg = new DependencyGraph();
+            Assert.AreEqual(0, dg.Size);
+        }
+    }
+}

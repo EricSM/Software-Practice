898b4ae5420d0a93d7cdbc4cbb99d81d1cb59a9c
EricSM <eric_miramontes@outlook.com>
2016-02-11 18:56:19 -0700

    Added back DependencyGraphTestCases and added new unit test project ProjectExtensionTestCases

---
+++ Spreadsheet/ProjectExtensionTestCases/ProjectExtensionTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectExtensionTestCases
+{
+    [TestClass]
+    public class ProjectExtensionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

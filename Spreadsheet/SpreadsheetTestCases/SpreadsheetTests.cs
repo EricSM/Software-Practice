a26ed999d1f62ae4c6f3dedf493db3af396f1b84
EricSM <eric_miramontes@outlook.com>
2016-02-19 00:25:52 -0700

    Finished renaming and commenting spreadsheet test file and methods

--- Spreadsheet/SpreadsheetTestCases/SpreadsheetTests.cs
+++ Spreadsheet/SpreadsheetTestCases/SpreadsheetTests.cs
@@ -10,11 +10,15 @@ using System.Collections.Generic;
 namespace SpreadsheetTestCases
 {
     [TestClass]
-    public class UnitTest1
+    public class SpreadsheetTests
     {        
+        /// <summary>
+        /// Tests for GetCellContents
+        /// </summary>
+
         [TestMethod]
         [ExpectedException(typeof(InvalidNameException))]
-        public void TestMethod1()
+        public void TestGetCellContents1()
         {
             Spreadsheet ss = new Spreadsheet();
             ss.GetCellContents(null);
@@ -22,14 +26,26 @@ namespace SpreadsheetTestCases
 
         [TestMethod]
         [ExpectedException(typeof(InvalidNameException))]
-        public void TestMethod2()
+        public void TestGetCellContents2()
         {
             Spreadsheet ss = new Spreadsheet();
             ss.GetCellContents("1a");
         }
 
         [TestMethod]
-        public void TestMethod3()
+        public void TestGetCellContents3()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            Assert.AreEqual(ss.GetCellContents("a1"), string.Empty);
+        }
+
+
+        /// <summary>
+        /// Tests for SetCellContents
+        /// </summary>
+
+        [TestMethod]
+        public void TestSetCellContents1()
         {
             Spreadsheet ss = new Spreadsheet();
             ss.SetCellContents("a1", 1);
@@ -37,29 +53,18 @@ namespace SpreadsheetTestCases
         }
 
         [TestMethod]
-        public void TestMethod4()
+        public void TestSetCellContents2()
         {
             Spreadsheet ss = new Spreadsheet();
             ss.SetCellContents("a1", "Hello World");
             Assert.AreEqual(ss.GetCellContents("a1"), "Hello World");
         }
 
-        [TestMethod]
-        public void TestMethod5()
-        {
-            Spreadsheet ss = new Spreadsheet();
-            for (int i = 1; i <= 5; i++)
-            {
-                ss.SetCellContents("a" + i, "Hello World");
-            }
-
-            var cells = new HashSet<string>(ss.GetNamesOfAllNonemptyCells());
-            Assert.IsTrue(cells.SetEquals(new HashSet<string> { "A1", "A2", "A3", "A4", "A5"}));
-        }
+        
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void TestMethod6()
+        public void TestSetCellContents3()
         {
             Spreadsheet ss = new Spreadsheet();
             ss.SetCellContents("a1", null);
@@ -67,7 +72,7 @@ namespace SpreadsheetTestCases
 
         [TestMethod]
         [ExpectedException(typeof(InvalidNameException))]
-        public void TestMethod7()
+        public void TestSetCellContents4()
         {
             Spreadsheet ss = new Spreadsheet();
             ss.SetCellContents("1a", "Hello World");
@@ -75,7 +80,7 @@ namespace SpreadsheetTestCases
 
         [TestMethod]
         [ExpectedException(typeof(InvalidNameException))]
-        public void TestMethod8()
+        public void TestSetCellContents5()
         {
             Spreadsheet ss = new Spreadsheet();
             ss.SetCellContents("1a", 0d);
@@ -83,14 +88,14 @@ namespace SpreadsheetTestCases
 
         [TestMethod]
         [ExpectedException(typeof(InvalidNameException))]
-        public void TestMethod9()
+        public void TestSetCellContents6()
         {
             Spreadsheet ss = new Spreadsheet();
             ss.SetCellContents("1a", new Formula());
         }
 
         [TestMethod]
-        public void TestMethod10()
+        public void TestSetCellContents7()
         {
             Spreadsheet ss = new Spreadsheet();
             ss.SetCellContents("a1", new Formula("a2"));
@@ -98,6 +103,22 @@ namespace SpreadsheetTestCases
             var cells = new HashSet<string>(ss.SetCellContents("a3", 0d));
 
             Assert.IsTrue(cells.SetEquals(new HashSet<string> { "A1", "A2", "A3" }));
+        }
+
+        /// <summary>
+        /// This method also tests GetNamesOfAllNonemptyCells
+        /// </summary>
+        [TestMethod]
+        public void TestGetNamesOfAllNonemptyCells1()
+        {
+            Spreadsheet ss = new Spreadsheet();
+            for (int i = 1; i <= 5; i++) // Set cells a1-5
+            {
+                ss.SetCellContents("a" + i, "Hello World");
+            }
+
+            var cells = new HashSet<string>(ss.GetNamesOfAllNonemptyCells()); // Retrieve them.
+            Assert.IsTrue(cells.SetEquals(new HashSet<string> { "A1", "A2", "A3", "A4", "A5" }));
         }
     }
 }

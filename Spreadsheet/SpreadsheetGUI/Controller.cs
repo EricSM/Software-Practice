c3641365f1c0592cdf8fa8c61ca56ce9bac8ad40
EricSM <eric_miramontes@outlook.com>
2016-02-29 20:13:11 -0700

    Set up MVC architecture as described in class examples.

---
+++ Spreadsheet/SpreadsheetGUI/Controller.cs
@@ -0,0 +1,23 @@
+﻿using SS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetGUI
+{
+    public class Controller
+    {
+        private ISpreadsheetView window;
+
+        private Spreadsheet spreadsheet;
+
+        public Controller(ISpreadsheetView window)
+        {
+            this.window = window;
+            this.spreadsheet = new Spreadsheet();
+
+        }
+    }
+}
--- Spreadsheet/SpreadsheetGUI/Form1.cs
+++ Spreadsheet/SpreadsheetGUI/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace SpreadsheetGUI
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form, ISpreadsheetView
     {
         public Form1()
         {
